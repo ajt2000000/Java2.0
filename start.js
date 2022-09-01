4d02e4f04b113bd059720612e3d9652ee8516d3c
ajt2000000 <111257318+ajt2000000@users.noreply.github.com>
2022-09-01 16:36:11 +0900

    Delete start.js

--- start.js
+++
@@ -1,9 +0,0 @@
-・・・
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js').then(async (registration) => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }).catch((err) => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-        }
-・・・
